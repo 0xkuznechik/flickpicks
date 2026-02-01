03213687c5eb7a95d2b90e508817114eed46e6c3
eox mooglevich massagetae <mf@mooglevich.dev>
2026-02-01 13:59:09 -0500

    flickpicks - turn off all cloudflare ip checking
    Note that there's lots of dead code for now
    
    BUILD=NO
    TEST=NO

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,8 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
-import { enforceCloudflareOnly } from "./utils/security.server";
+// Cloudflare security check - disabled for now but kept for future use
+// import { enforceCloudflareOnly } from "./utils/security.server";
 
 export default function handleRequest(
   request: Request,
@@ -9,11 +10,11 @@ export default function handleRequest(
   headers: Headers,
   context: EntryContext
 ) {
-  // Enforce Cloudflare-only traffic in production
-  const securityResponse = enforceCloudflareOnly(request);
-  if (securityResponse) {
-    return securityResponse;
-  }
+  // Cloudflare-only enforcement - disabled
+  // const securityResponse = enforceCloudflareOnly(request);
+  // if (securityResponse) {
+  //   return securityResponse;
+  // }
 
   const markup = renderToString(
     <RemixServer context={context} url={request.url} />
