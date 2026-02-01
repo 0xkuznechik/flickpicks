94cdc02f2faf3332104f0e54e5c29902618c3e59
eox mooglevich massagetae <mf@mooglevich.dev>
2026-02-01 00:05:25 -0500

    flickpicks - removed cloudflare references, local dev works
    BUILD=PASS
    TEST=PASS

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import {
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-  vitePlugin as remix,
-} from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxy(), // Enables Cloudflare emulation locally
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [remix()],
   server: {
     port: 5173,
     strictPort: true,
